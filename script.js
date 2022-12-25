09149a9d6174c0484d0dc2b52a905c26d91aa54f
Sourav Kumar <souravsharma2102@gmail.com>
2022-12-26 00:38:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function btnToggle() {
+    var menuWork = document.getElementById("hidden");
+    if (menuWork.style.display === "none") {
+      menuWork.style.display = "block";
+    } else {
+      menuWork.style.display = "none";
+    }
+}
